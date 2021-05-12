46337fc65fd6e27151e69f52e2f3ca723e0d9c4c
Isidore <isidore.porretti@epfedu.fr>
2021-05-12 22:00:44 +0200

    'correction_problème_cartes_qui_ne_se_retournent_pas'

--- front-end/src/app/components/game/card/card.component.js
+++ front-end/src/app/components/game/card/card.component.js
@@ -5,56 +5,56 @@ import "./card.component.scss";
 import template from "./card.component.html";
 
 
-
-
 export class CardComponent extends Component {
 
     
     
-        constructor(id) {
-            super("card");
-        
-            // is this card flipped ?
-        this._flipped = false;
-
-        // has the matching card has been discovered already ?
-        this.matched = false;
-
-        this._id = id;
-
-        this._elt = super.getElement();
-        this._imageElt = this._elt.querySelector('.card-wrapper');
-
-
-            // ...
-            this._imageElt.querySelector(
-              "img.front-face"
-            ).src = `src/app/components/game/card/assets/card-${this._id}.png`;
-            this._imageElt.querySelector("img.back-face").src =
-              "src/app/components/game/card/assets/back.png";
-          }
-
-
-          getTemplate() {
-            return template;
-        }
-        
+    constructor(id) {
+        super("card");
     
-        flip() {
-            this._imageElt.classList.toggle('flip');
-            this._flipped = !this._flipped;
-        }
-    
-        equals(card) {
-            return card._id === this._id;
-        }
-    
+        // is this card flipped ?
+    this._flipped = false;
 
-        get flipped() {
-            return this._flipped;
-        }
-        
+    // has the matching card has been discovered already ?
+    this.matched = false;
+
+    this._id = id;
+
+    this._elt = super.getElement();
+    this._imageElt = this._elt.querySelector('.card-wrapper');
+
+
+        // ...
+        this._imageElt.querySelector(
+          "img.front-face"
+        ).src = `src/app/components/game/card/assets/card-${this._id}.png`;
+        this._imageElt.querySelector("img.back-face").src =
+          "src/app/components/game/card/assets/back.png";
+      }
+
+
+      getTemplate() {
+        return template;
     }
+    
+
+    flip() {
+        this._imageElt.classList.toggle('flip');
+        this._flipped = !this._flipped;
+    }
+
+    equals(card) {
+        return card._id === this._id;
+    }
+
+
+    get flipped() {
+        return this._flipped;
+    }
+    
+}
+
+
 
     // put component in global scope, tu be runnable right from the HTML.
  
--- front-end/src/app/components/game/game.component.js
+++ front-end/src/app/components/game/game.component.js
@@ -1,151 +1,141 @@
 // front-end/src/app/components/game/game.component.js
 import { Component } from "../../utils/component";
 import { parseUrl } from "../../utils/utils";
-
 import { CardComponent } from "./card/card.component";
 import template from "./game.component.html";
 
+const environment = {
+    api: {
+        host: 'http://localhost:8081'
+    }
+};
 
-
-
-    const environment = {
-        api: {
-            host: 'http://localhost:8081'
-        }
-    };
 
   
-      
-    export class GameComponent extends Component {
-        
-        getTemplate() {
-            return template;
-          }
-        
-        constructor() {
-            super("game");
+export class GameComponent extends Component {
+    
+    getTemplate() {
+        return template;
+      }
+    
+    constructor() {
+        super("game");
 
-            // gather parameters from URL
-            const params = parseUrl();
+        // gather parameters from URL
+        const params = parseUrl();
 
-            // save player name & game ize
-            this._name = params.name;
-            this._size = parseInt(params.size) || 9;
-            this._flippedCard = null;
-            this._matchedPairs = 0;
-        }
+        // save player name & game ize
+        this._name = params.name;
+        this._size = parseInt(params.size) || 9;
+        this._flippedCard = null;
+        this._matchedPairs = 0;
+    }
 
-        async init() {
-            // fetch the cards configuration from the server
-            const config = await this.fetchConfig() 
-                this._config = config;
+    async init() {
+        // fetch the cards configuration from the server
+        const config = await this.fetchConfig() 
+            this._config = config;
 
-                // create a card out of the config    
-                this._cards = this._config.ids.map((id) => {
-                    return new CardComponent(id)
-                })
-
-                this._boardElement = document.querySelector('.cards');
-
-
-                this._cards.forEach(card => {
-                    this._boardElement.appendChild(card.getElement());
-                    card.getElement().addEventListener('click',  () => { this._flipCard(card) });
-                });
-
-                this.start();
-            ;
-        }
-
-        start() {
-            this._startTime = Date.now();
-            let seconds = 0;
-            
-            document.querySelector('nav .navbar-title').textContent = `Player:${this._name}. Elapsed time:${seconds++}`;
-
-            this._timer = setInterval(()=> { 
-
-                document.querySelector('nav .navbar-title').textContent = `Player:${this._name}. Elapsed time:${seconds++}`;
-            } , 1000);
-        }
-
-        gotoScore() {
-            const timeElapsedInSeconds = Math.floor(
-              (Date.now() - this._startTime) / 1000
-            );
-          
-            setTimeout(() => window.location.hash = `score?name=${this._name}&size=${this._size}'&time=${timeElapsedInSeconds}`, 750);
-          }
-          
-
-        async fetchConfig() {
-
-            return fetch(`${environment.api.host}/board?size=${this. _size}`, {
-              method: "GET",
+            // create a card out of the config    
+            this._cards = this._config.ids.map((id) => {
+                return new CardComponent(id)
             })
-              .then((response) => response.json())
-              .catch((error) => console.log("Error while fetching   config: ", error));
-          }
-          
 
-        _flipCard(card) {
-            if (this._busy) {
-                return;
-            }
-
-            if (card.flipped) {
-                return;
-            }
+            this._boardElement = document.querySelector('.cards');
 
 
-            // flip the card
-            card.flip();
+            this._cards.forEach(card => {
+                this._boardElement.appendChild(card.getElement());
+                card.getElement().addEventListener('click',  () => { this._flipCard(card) });
+            });
 
-            // if flipped first card of the pair
-            if (!this._flippedCard) {
-                // keep this card flipped, and wait for the second card of the pair
-                this._flippedCard = card;
+            this.start();
+        ;
+    }
+
+    start() {
+        this._startTime = Date.now();
+        let seconds = 0;
+        
+        document.querySelector('nav .navbar-title').textContent = `Player:${this._name}. Elapsed time:${seconds++}`;
+
+        this._timer = setInterval(()=> { 
+
+            document.querySelector('nav .navbar-title').textContent = `Player:${this._name}. Elapsed time:${seconds++}`;
+        } , 1000);
+    }
+
+    gotoScore() {
+        const timeElapsedInSeconds = Math.floor(
+          (Date.now() - this._startTime) / 1000
+        );
+      
+        setTimeout(() => window.location.hash = `score?name=${this._name}&size=${this._size}'&time=${timeElapsedInSeconds}`, 750);
+      }
+      
+
+    async fetchConfig() {
+
+        return fetch(`${environment.api.host}/board?size=${this. _size}`, {
+          method: "GET",
+        })
+          .then((response) => response.json())
+          .catch((error) => console.log("Error while fetching   config: ", error));
+      }
+      
+
+    _flipCard(card) {
+        if (this._busy) {
+            return;
+        }
+
+        if (card.flipped) {
+            return;
+        }
+
+
+        // flip the card
+        card.flip();
+
+        // if flipped first card of the pair
+        if (!this._flippedCard) {
+            // keep this card flipped, and wait for the second card of the pair
+            this._flippedCard = card;
+        } else {
+            // second card of the pair flipped...
+
+            // if cards are the same
+            if (card.equals(this._flippedCard)) {
+                this._flippedCard.matched = true;
+                card.matched = true;
+                this._matchedPairs += 1;
+
+                // reset flipped card for the next turn.
+                this._flippedCard = null;
+
+                if (this._matchedPairs === this._size) {
+                    this.gotoScore();
+                }
             } else {
-                // second card of the pair flipped...
+                this._busy = true;
 
-                // if cards are the same
-                if (card.equals(this._flippedCard)) {
-                    this._flippedCard.matched = true;
-                    card.matched = true;
-                    this._matchedPairs += 1;
+                // cards did not match
+                // wait a short amount of time before hiding both cards
+                
+                setTimeout( ()=> {
+                    // hide the cards
+                    this._flippedCard.flip();
+                    card.flip();
+                    this._busy = false;
 
                     // reset flipped card for the next turn.
                     this._flippedCard = null;
-
-                    if (this._matchedPairs === this._size) {
-                        this.gotoScore();
-                    }
-                } else {
-                    this._busy = true;
-
-                    // cards did not match
-                    // wait a short amount of time before hiding both cards
-                    
-                    setTimeout( ()=> {
-                        // hide the cards
-                        this._flippedCard.flip();
-                        card.flip();
-                        this._busy = false;
-
-                        // reset flipped card for the next turn.
-                        this._flippedCard = null;
-                    } ,500);
-                }
+                } ,500);
             }
         }
-
     }
 
-
-    
-
-
-
+}
 
    
 
